6307e34e5292ac762936cd86357fd4c5f51005e2
Your Name <ali18@wgu.edu>
2022-01-18 21:23:19 -0800

    commit

---
+++ src/pages/{mdx slug}.js
@@ -0,0 +1,25 @@
+{
+    allMdx {
+      nodes {
+        slug
+      }
+    }
+  }
+  {
+    "data": {
+      "allMdx": {
+        "nodes": [
+          {
+            "slug": "my-first-post"
+          },
+          {
+            "slug": "yet-another-post"
+          },
+          {
+            "slug": "another-post"
+          }
+        ]
+      }
+    },
+    "extensions": {}
+  }
