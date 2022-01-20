8acd4ce70561146cab95bc059768e9c7547c9bfd
Your Name <ali18@wgu.edu>
2022-01-19 23:57:45 -0800

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,13 +40,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-markdown',
-        short_name: 'starter',
+        name: 'amaterasu',
+        short_name: 'amaterasu',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/amaterasu.jpg', // This path is relative to the root of the site.
       },
     },
     {
