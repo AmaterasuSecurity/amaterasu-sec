0197c2d609262d68b612cad5c8428ec6fcec5ed5
dr0pp3dpack3ts <ali18@wgu.edu>
2022-01-19 16:34:12 -0800

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { Link } from 'gatsby'
 import { Button } from 'antd'
 import { GithubOutlined, LinkedinOutlined  } from '@ant-design/icons'
-import amaterasu from '../static'
+import amaterasu from '../static/amaterasu'
 
 
 
