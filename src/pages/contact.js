a5862116a6549a85a70c503eb870b08622c0251b
dr0pp3dpack3ts <ali18@wgu.edu>
2022-03-08 15:31:39 -0800

    commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,6 @@
 import React, { useState }  from "react"
 import axios from "axios";
-import { Link } from "gatsby"
-import Layout from "../components/layout"
+import Layout from "../templates/Layout"
 
 
 const MyForm = () => {
